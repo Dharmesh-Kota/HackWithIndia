9c58e8b4f408cb4c0166121ab9cc955758137124
ManojRDhundhalva <manojrdhundhalva@gmail.com>
2024-04-02 02:20:08 +0530

    login-register-profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Profile from "./pages/Profile";
 import Store from "./pages/Store";
 import History from "./pages/History";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -12,10 +15,13 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
         <Route exact path="/profile" element={<Profile />} />
         <Route exact path="/store" element={<Store />} />
         <Route exact path="/history" element={<History />} />
       </Routes>
+      <Footer />
     </>
   );
 }
