dd6ff0158bfe7f602731fee580974b4fba010833
ManojRDhundhalva <manojrdhundhalva@gmail.com>
2024-04-02 19:36:28 +0530

    login-register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,22 +8,23 @@ import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Footer from "./components/Footer";
+import { useAuth } from "./context/auth";
 
 function App() {
+  const { isLoggedIn } = useAuth();
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/profile" element={<Profile />} />
-        <Route exact path="/register" element={<Register />} />
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/profile" element={<Profile />} />
         <Route exact path="/store" element={<Store />} />
         <Route exact path="/history" element={<History />} />
-        <Route exact path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </>
   );
 }
-
 export default App;
